720b6b73cb5fc8c0ce942a397e6badd6a8f35eeb
Ben Ford <ben.ford@puppetlabs.com>
2020-06-01 19:10:56 -0700

    update queries

--- modules_per_environment.sql
+++ modules_per_environment.sql
@@ -1,5 +1,8 @@
 -- This will generate an aggregate table consisting of the number of modules
 -- in and environment and how many instances of that count there are.
-SELECT ARRAY_LENGTH(modules) count, COUNT(1) AS occurrences
-FROM `bto-dataops-datalake-prod.dujour.community_metrics`
+-- The subselect ensures uniqueness without exposing site_id
+SELECT count, COUNT(1) AS occurrences
+FROM (
+  SELECT DISTINCT(site_id), ARRAY_LENGTH(modules) as count FROM `bto-dataops-datalake-prod.dujour.community_metrics`
+)
 GROUP BY count
