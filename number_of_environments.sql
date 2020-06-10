ad7cc9788f4350a4f5325e8b69b29ecd1bdc1b2c
Ben Ford <ben.ford@puppetlabs.com>
2020-06-10 16:47:37 -0700

    Delete number_of_environments.sql

--- number_of_environments.sql
+++
@@ -1,5 +0,0 @@
--- This will generate an aggregate table consisting of the number of environments
--- exist in infrastructures and how many occurrences of that count there are.
-SELECT environment_count, count(1) AS occurrences
-FROM `dataops-puppet-public-data.community.community_metrics`
-GROUP BY environment_count
